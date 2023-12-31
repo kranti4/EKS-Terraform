87c2fcbbfefd17211960d457a681851df77497a3
kranti4 <146917885+kranti4@users.noreply.github.com>
2023-12-31 15:19:47 +0530

    Update eks_node_grp.tf

--- eks_node_grp.tf
+++ eks_node_grp.tf
@@ -22,8 +22,8 @@ resource "aws_eks_node_group" "node-grp" {
   instance_types  = ["t2.small"]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
 
