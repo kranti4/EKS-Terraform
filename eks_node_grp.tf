e52609c3bd72e3f97fcccdc5cb83f019cfab2805
kranti4 <146917885+kranti4@users.noreply.github.com>
2024-01-02 11:41:46 +0530

    Update eks_node_grp.tf

--- eks_node_grp.tf
+++ eks_node_grp.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "kubectl-server" {
+resource "aws_instance" "eks-terr" {
   ami                         = "ami-0c7217cdde317cfec"
   key_name                    = "ekscluster"
   instance_type               = "t2.small"
@@ -7,14 +7,14 @@ resource "aws_instance" "kubectl-server" {
   vpc_security_group_ids      = [aws_security_group.allow_tls.id]
 
   tags = {
-    Name = "kubectl"
+    Name = "eks"
   }
 
 }
 
 resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "pc-node-group"
+  node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.worker.arn
   subnet_ids      = [aws_subnet.public-1.id, aws_subnet.public-2.id]
   capacity_type   = "ON_DEMAND"
