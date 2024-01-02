c86f31833af24deb6138f505233e256ec7f5aa71
kranti4 <146917885+kranti4@users.noreply.github.com>
2024-01-02 15:05:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,5 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  access_key = "AKIAVYJFBYOMAOH4PS2D"
-  secret_key = "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"
   region = "us-east-1"
 }
